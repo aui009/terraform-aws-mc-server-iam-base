7c77a188b537f9a19fe7582f26ac41c5b83010d1
Aui Santos <auisantos09@gmail.com>
2026-04-06 16:46:23 +0800

    feat(iam): add new iam role and policies

--- output.tf
+++ output.tf
@@ -1,4 +1,17 @@
 ##############################################################
+# Outputs for IAM Roles
+##############################################################
+output "mc_server_role_arn" {
+  description = "ARN of the IAM role for Minecraft server EC2 instance"
+  value       = aws_iam_role.mc_server_role.arn
+}
+
+output "step_fn_role_ssm_ec2_role_arn" {
+  description = "ARN of the IAM role for Step Function to manage SSM and EC2"
+  value       = aws_iam_role.step_fn_role_ssm_ec2_role.arn
+}
+
+##############################################################
 # Outputs for IAM policies
 ##############################################################
 
